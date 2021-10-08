da67ff943fa648013df0f24eee1551df6144a85b
nayomi16 <nayomirashmikaaa@gmail.com>
2021-10-09 02:11:04 +0530

    added serializers

--- system/serializers.py
+++ system/serializers.py
@@ -1,9 +1,52 @@
 # serializers.py
 from rest_framework import serializers
 
-from .models import TeamModel
+from .models import CoachModel, GameModel, PlayerModel, PlayerStatModel, RoleModel, TeamModel, TeamStatModel, UserRoleModel, UserStatModel
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = RoleModel
+        fields = ('type')
+
+class UserRoleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserRoleModel
+        fields = ('user', 'role', 'is_logged_in')
+
+class UserStatSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserStatModel
+        fields = ('user','login_time', 'logout_time')
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TeamModel
         fields = ('name', 'abbr')
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = GameModel
+        fields = ('compet_team01', 'compet_team02', 'compet1_score', 'compet2_score', 'winner', 'date', 'round_number')
+
+class TeamStatSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TeamStatModel
+        fields = ('team', 'game', 'score')
+
+class CoachSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CoachModel
+        fields = ('user', 'team')
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PlayerModel
+        fields = ('user', 'team', 'height')
+
+class PlayerStaterializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PlayerStatModel
+        fields = ('player', 'game', 'score')
+
+
+
